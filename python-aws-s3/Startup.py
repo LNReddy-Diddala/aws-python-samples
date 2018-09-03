bb878392e352e707a4c379fb46a92174877ef8f9
MSRIT <msr@localhost.localdomain>
2018-09-03 19:13:19 +0530

    AWS-Python some more operations added.

--- python-aws-s3/Startup.py
+++ python-aws-s3/Startup.py
@@ -2,13 +2,26 @@ import boto3
 from botocore.exceptions import ClientError
 
 
+REGION = 'us-east-2'
+BUCKET_NAME = 'test-bucket-msr560'
+UPLOAD_FILE_PATH = '/home/msr/s3-python-test_latest.txt' #Place your file-pathe along with file-name
+UPLAOD_FILE_NAME = 's3-python-test_latest.txt'
+FILE_NAME_IN_S3 = 's3-python-test_latest.txt'
+DOWNLOAD_FILE_NAME = 's3-python-test_latest.txt'
+DELETE_FILE_NAME = 's3-python-test_latest.txt'
+DELETE_BUCKET_NAME = 'test-bucket-msr560'
+
 def main():
 
-	REGION = 'us-east-2'
 	# Create an S3 client
 	s3 = boto3.client('s3')
+	list_s3_buckets(s3)
+	create_s3_bucket(s3, BUCKET_NAME, REGION)
+	#upload_file_to_bucket(s3, UPLOAD_FILE_PATH, BUCKET_NAME, UPLAOD_FILE_NAME)
+	#download_file_from_bucket(BUCKET_NAME, FILE_NAME_IN_S3, UPLAOD_FILE_NAME)
 	#list_s3_buckets(s3)
-	create_s3_bucket(s3, 'test-bucket-msr560', REGION)
+	#delete_file_from_bucket(BUCKET_NAME, DELETE_FILE_NAME) # Still pending------>
+	#delete_bucket(DELETE_BUCKET_NAME)
 	#list_s3_buckets(s3)
 
 # Get list of buckets
@@ -28,33 +41,69 @@ def create_s3_bucket(s3, bucket_name, REGION):
 
 	try:
 		response = s3.create_bucket(Bucket = bucket_name, CreateBucketConfiguration = {'LocationConstraint': REGION})
+
 		if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-			print("{bucket} bucket is created successfully.".format(bucket = bucket_name))
+			print('"{bucket}" bucket has been created successfully.'.format(bucket = bucket_name))
+
+	except ClientError as e:
+			print(e.response['Error']['Message'])
+
+# Upload file to the bucket
+def upload_file_to_bucket(S3, file_path, bucket_name, file_name):
+
+	S3.upload_file(file_path, bucket_name, file_name)
+	print('"{file_name}" file has been uploaded to "{bucket}" bucket successfully'.format(file_name = file_name, bucket = bucket_name))
+	
+
+# Download file from bucket
+def download_file_from_bucket(bucket_name, file_name_in_s3, file_name):
+
+	s3 = boto3.resource('s3')
+
+	try:
+		s3.Bucket(bucket_name).download_file(file_name_in_s3, file_name)
+		print('"{file_name}" file has been downloaded from "{bucket} successfully"'.format(file_name = file_name_in_s3, bucket = bucket_name))
 
 	except ClientError as e:
 
-		if e.response['Error']['Code'] == 'BucketAlreadyExists':
-			print(e.response['Error']['Message'])
-		elif e.response['Error']['Code'] == 'IllegalLocationConstraintException':
-			print(e.response['Error']['Message'])
-		else:
-			print(e.response)
+	    if e.response['Error']['Code'] == "404":
+	        print("The object does not exist.")
+	    else:
+	        raise
 
-# Upload file to the bucket
-def upload_file_to_bucket():
-	pass
-
-# Download file from bucket
-def upload_file_to_bucket():
-	pass
 
 # Delete file from bucket
-def delete_file_from_bucket():
-	pass
+def delete_file_from_bucket(bucket_name, delete_file_name):
+	s3 = boto3.resource('s3')
+
+	try:
+		s3.Bucket(bucket_name).delete_objects(delete_file_name)
+		print('"{file_name}" file has been deleted from "{bucket} successfully"'.format(file_name = delete_file_name, bucket = bucket_name))
+
+	except ClientError as e:
+
+	    if e.response['Error']['Code'] == "404":
+	        print("The object does not exist.")
+	    else:
+	        raise
 
 # Delete bucket, if bucket exist
-def delete_bucket():
-	pass
+def delete_bucket(bucket_name):
+
+	try:
+		s3 = boto3.resource('s3')
+		bucket = s3.Bucket(bucket_name)
+		for key in bucket.objects.all():
+			key.delete()
+
+		response = bucket.delete()
+
+		if response['ResponseMetadata']['HTTPStatusCode'] == 204:
+			print('"{bucket}" bucket has been deleted successfully.'.format(bucket = bucket_name))
+
+	except ClientError as e:
+		print(e.response['Error']['Message'])
+
 
 
 if __name__ == '__main__':
